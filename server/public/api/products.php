f8d9efe2815dcafd34d48f09d542f0abd348fba9
cresia <kimkim4@gmail.com>
2019-08-24 10:13:59 -0700

    connect-product-table

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -1,26 +1,35 @@
 <?php
 
 header('Content-Type: application/json');
-require_once('db_connection.php');
 require_once('functions.php');
+set_exception_handler('error_handler');
+set_error_handler('error_handler');
+require_once('db_connection.php');
 
 // http_response_code(500);
 // dostuff();
-set_exception_handler('error_handler');
-set_error_handler('error_handler');
+
 
 startup();
 
+// if(empty($_GET['id']){
+//   $WhereClause = readFile('dummy-products-list.json');
+// }
+// else{
+//   $WhereClause = "WHERE `id` = 3";
+// }
 
-$query = "SELECT * FROM `products`";
-$result = mysqli_query($conn, $query);
+
+// $query = "SELECT * FROM `products` {$WhereClause}";
+
+
+$result = mysqli_query($conn, $query); //send the query to conn
 if (!$result) {
-  print("Error: " . mysqli_error($db));
-  exit();
+  throw new Exception("Connect failed: " . mysqli_error());
 }
 
 
-$output = [];
+$output = array();
 while ($row = mysqli_fetch_assoc($result)) {
   $output[] = $row;
 }
@@ -30,8 +39,8 @@ print(json_encode($output));
 if(!$conn){
   throw new Exception("Error:" . mysqli_connect_error());
 };
-$output = file_get_contents('dummy-products-list.json');
-print($output);
+// $output = file_get_contents('dummy-products-list.json');
+// print($output);
 
 
 ?>
